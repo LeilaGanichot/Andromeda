5261a799e81256f28ddfffffb0ec3f367d05d959
Haruhiism <ccnopbl@gmail.com>
2022-04-07 16:27:40 -0500

    task/to-do-fragment - add navigation components graph, set up initial navigation between bottom navigation bar & corresponding destinations, begin work on to-do fragment UI

--- app/src/main/java/edu/fsu/cs/andromeda/ui/todo/ToDoFragment.java
+++ app/src/main/java/edu/fsu/cs/andromeda/ui/todo/ToDoFragment.java
@@ -1,18 +1,38 @@
 package edu.fsu.cs.andromeda.ui.todo;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.joda.time.LocalDate;
+import org.joda.time.YearMonth;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.fsu.cs.andromeda.R;
-import edu.fsu.cs.andromeda.db.note.Note;
 import edu.fsu.cs.andromeda.db.todo.ToDo;
+import edu.fsu.cs.andromeda.db.todo.ToDoViewModel;
+import edu.fsu.cs.andromeda.ui.todo.calendar.CalendarAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +40,26 @@ import edu.fsu.cs.andromeda.db.todo.ToDo;
  * create an instance of this fragment.
  */
 public class ToDoFragment extends Fragment {
+
+    // Widgets
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView, rvDayDetails;
+    private TextView tvCourseTest, tvAssignmentTest;
+
+    private FloatingActionButton fabAddNewToDo;
+
+    // top calendar controls
+    private CalendarAdapter calendarAdapter;
+    private Button btnPrevMonth, btnNextMonth;
+    // calendar holder
+    private LinearLayout llCalendarHolder;
+
+    // Local vars
+    private org.joda.time.LocalDate selectedDate;
+    // screen's dimensions
+    int sWidth;
+    int sHeight;
+    private ToDoViewModel toDoViewModel;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,13 +103,89 @@ public class ToDoFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        toDoViewModel = new ViewModelProvider(this).get(ToDoViewModel.class);
+
+        // get the device dimensions to change the display constraints dynamically
+        sWidth = getContext().getResources().getDisplayMetrics().widthPixels;
+        sHeight = getContext().getResources().getDisplayMetrics().heightPixels;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do, container, false);
+        View view =  inflater.inflate(R.layout.fragment_to_do, container, false);
+
+        defineWidgets(view);
+        //setCalendarViews();
+        setOnClickListeners();
+
+        return view;
+    }
+
+    private void defineWidgets(View view) {
+        calendarRecyclerView = view.findViewById(R.id.rv_calendar);
+        rvDayDetails = view.findViewById(R.id.rv_day_details);
+        monthYearText = view.findViewById(R.id.tv_month_year);
+
+        btnPrevMonth = view.findViewById(R.id.btn_prev_month);
+        btnNextMonth = view.findViewById(R.id.btn_next_month);
+
+        llCalendarHolder = view.findViewById(R.id.ll_calendar_holder);
+        llCalendarHolder.setLayoutParams(new LinearLayout.LayoutParams(sWidth, sHeight));
+
+        fabAddNewToDo = view.findViewById(R.id.fab_add_new_to_do);
+    }
+
+    private void setCalendarViews() {
+        // month view
+        monthYearText.setText(monthYearFromDate(selectedDate));
+
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+        calendarAdapter = new CalendarAdapter(daysInMonth, monthFromDate(selectedDate));
+
+        if(toDoViewModel != null) {
+            toDoViewModel.getAllToDosByDueDate().observe(
+                    getViewLifecycleOwner(),
+                    toDos -> calendarAdapter.setAllToDos(toDos)
+            );
+        }
+
+        // rv stuff
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 7); // 7 columns in the rv
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+        calendarAdapter.setOnItemClickListener(new CalendarAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, String dayText) {
+                if(!dayText.equals(" ")){
+
+                }
+            }
+        });
+
+        rvDayDetails.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvDayDetails.setHasFixedSize(true);
+        // rvDayDetails.setAdapter(); TODO @cm set adapter of To Dos
+    }
+
+    private String monthYearFromDate(org.joda.time.LocalDate selectedDate) {
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("MMMM yyyy");
+        return dtf.print(selectedDate);
+    }
+
+    private ArrayList<String> daysInMonthArray(org.joda.time.LocalDate selectedDate) {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        // TODO @cm probably should switch, Joda time is being uncooperative
+        return daysInMonthArray;
+    }
+
+    private String monthFromDate(LocalDate selectedDate) {
+        return null;
+    }
+
+    private void setOnClickListeners() {
     }
 
     @Override
---
+++ app/src/main/java/edu/fsu/cs/andromeda/ui/todo/calendar/CalendarAdapter.java
@@ -0,0 +1,96 @@
+package edu.fsu.cs.andromeda.ui.todo.calendar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+import edu.fsu.cs.andromeda.R;
+import edu.fsu.cs.andromeda.db.todo.ToDo;
+
+public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder> {
+
+    private final ArrayList<String> daysOfMonth;
+    private String month;
+    private OnItemClickListener listener;
+
+    private List<ToDo> allToDos = new ArrayList<>();
+
+    public CalendarAdapter(ArrayList<String> daysOfMonth, String month) {
+        this.daysOfMonth = daysOfMonth;
+        this.month = month;
+    }
+
+    @NonNull
+    @Override
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_calendar_cell, parent, false);
+
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) (parent.getHeight() * 0.1666666); // get each cell to be exactly 1/6th of the view
+
+        return new CalendarViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+        holder.tvDayOfMonth.setText(daysOfMonth.get(position));
+
+
+        if(!allToDos.isEmpty()) setDueDateIndicator(holder, position);
+    }
+
+    private void setDueDateIndicator(CalendarViewHolder holder, int position) {
+        for (ToDo toDo: allToDos) {
+            if(toDo.getDueDate() != null){
+                // TODO figure out the below for API < 26
+//                if(extractDayFromDate(toDo.getDueDate()).equals(daysOfMonth.get(position))){
+//                    holder.tvDueDateIndicator.setVisibility(View.VISIBLE);
+//                }
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return daysOfMonth.size();
+    }
+
+    public void setAllToDos(List<ToDo> allToDos){
+        this.allToDos = allToDos;
+        notifyDataSetChanged();
+    }
+
+    public class CalendarViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvDayOfMonth;
+        private TextView tvDueDateIndicator;
+
+        public CalendarViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvDayOfMonth = itemView.findViewById(R.id.tv_cell_day_text);
+            tvDueDateIndicator = itemView.findViewById(R.id.tv_to_do_indicator);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onItemClick(getAdapterPosition(), tvDayOfMonth.getText().toString());
+                }
+            });
+        }
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position, String dayText);
+    }
+}
+
